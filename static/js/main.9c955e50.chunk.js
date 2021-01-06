383632b29f19205279c7a820aad6d3a258c2df21
Jack Bruck <jackbruck@Jacks-MacBook-Air.local>
2021-01-06 12:25:51 -0600

    Updates

--- static/js/main.9c955e50.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgh-page"]=this["webpackJsonpgh-page"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(2),i=n(0),a=n.n(i),o=n(3),s=n.n(o),d=(n(9),n.p+"static/media/App.7e3cdddc.babel"),g=function(e){e&&e instanceof Function&&n.e(3).then(n.bind(null,11)).then((function(t){var n=t.getCLS,c=t.getFID,i=t.getFCP,a=t.getLCP,o=t.getTTFB;n(e),c(e),i(e),a(e),o(e)}))};s.a.render(Object(c.jsx)(a.a.StrictMode,{children:Object(c.jsx)(d,{})}),document.getElementById("root")),g()},9:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.9c955e50.chunk.js.map
